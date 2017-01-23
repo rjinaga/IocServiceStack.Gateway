1c6bc4ff76b8c6ca5a9495a64b7ec9075f3a34e5
rjinaga <rjinaga@outlook.com>
2017-01-23 21:04:42 +0530

    Update changes as per IocServiceStack 1.0.0

--- src/IocServiceStack.Gateway/ServiceActionsDictionary.cs
+++ src/IocServiceStack.Gateway/ServiceActionsDictionary.cs
@@ -25,15 +25,8 @@
 
 namespace IocServiceStack.Gateway
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Linq.Expressions;
-    using System.Reflection;
-    using System.Threading.Tasks;
-
-  
-
+    
     internal class ServiceActionsDictionary : Dictionary<string, ServiceActionDescriptor>
     {
         static ServiceActionsDictionary _singleton;
